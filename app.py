2f0ef6735755e6871157976151211520dbd27e9a
Alex Sulsky <alex.sulsky@gmail.com>
2017-10-20 16:41:40 -0500

    boilerplate db added

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 from flask import Flask, render_template
+import os
+from main import app
 
-app = Flask(__name__)
+# Make sure to have in your .env file, the following:
+# source env/bin/activate
+# Then
+# export APP_SETTINGS="config.DevelopmentConfig"
+# Run the following to update then refresh your .bashrc:
+# $ echo "source `which activate.sh`" >> ~/.bashrc
+# $ source ~/.bashrc
+from models import Result
 
 
-@app.route('/', methods=('GET', 'POST'))
-def hello():
-    return render_template('index.html')
+def main():
 
 
-if __name__ == "__main__":
-    app.run('0.0.0.0', port=5000, debug=True)
+    @app.route('/', methods=('GET', 'POST'))
+    def hello():
+        return render_template ('index.html')
+
+
+    @app.route('/<name>')
+    def hello_name(name):
+        return "Hello {}!".format(name)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
