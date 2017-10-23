06eea09d87899a5c8ce1e0497cd42e42acf80a89
Lora Griffin <lora.jean@me.com>
2017-10-23 15:37:47 -0500

    updated main.js to remove scrolling jquery)

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,5 @@
 var map;
+
 function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
     zoom: 10,
@@ -36,25 +37,6 @@ $(document).ready(function () {
     console.log('sidebar toggle');
     $('#sidebar').toggleClass('active');
 
-    $('a[href^="#"]').click(function() {
-        // target element id
-        var id = $(this).attr('href');
-
-        // target element
-        var $id = $(id);
-        if ($id.length === 0) {
-            return;
-        }
-
-        // prevent standard hash navigation (avoid blinking in IE)
-        event.preventDefault();
-
-        // top position relative to the document
-        var pos = $id.offset().top;
-
-        // animated top scrolling
-        $('body, html').animate({scrollTop: pos});
-    });
   });
 
 });
